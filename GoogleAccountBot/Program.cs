82e4c9778811ac2ac84ad00cf487962668b2369b
Dung <Dung@DEVELOPER>
2016-12-26 00:15:15 +0700

    Add project files.

---
+++ GoogleAccountBot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace GoogleAccountBot
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("user-data-dir=C:/Users/Dung/Desktop/User Data");
+            options.AddArgument("--start-maximized");
+            ChromeDriver driverOne = new ChromeDriver(options);
+            driverOne.Navigate().GoToUrl("http://192.168.1.1:8080");
+        }
+    }
+}
